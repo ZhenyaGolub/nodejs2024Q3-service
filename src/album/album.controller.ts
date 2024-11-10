b01a7a435ee4a5630271ce902e068ed26f847951
Zhenya Holub <zhenya.holub@rdigital.by>
2024-11-10 17:26:43 +0300

    feat: create album controller and service

--- src/album/album.controller.ts
+++ src/album/album.controller.ts
@@ -1,12 +1,46 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { AlbumService } from './album.service';
+import { Album, ValidatedAlbum } from './types/album.types';
 
-@Controller()
+@Controller('album')
 export class AlbumController {
   constructor(private readonly albumService: AlbumService) {}
 
   @Get()
-  getHello(): string {
-    return '';
+  getAll(): Album[] {
+    return this.albumService.getAll();
+  }
+
+  @Get(':id')
+  getOne(@Param('id') id: string): Album {
+    return this.albumService.getOne(id);
+  }
+
+  @Post()
+  create(@Body() createAlbumDto: ValidatedAlbum): Album {
+    return this.albumService.create(createAlbumDto);
+  }
+
+  @Put(':id')
+  update(
+    @Body() updateAlbumDto: ValidatedAlbum,
+    @Param('id') id: string,
+  ): Album {
+    return this.albumService.update(updateAlbumDto, id);
+  }
+
+  @Delete(':id')
+  @HttpCode(204)
+  delete(@Param('id') id: string) {
+    return this.albumService.delete(id);
   }
 }
--- src/album/album.service.ts
+++ src/album/album.service.ts
@@ -1,8 +1,70 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Album, ValidatedAlbum } from './types/album.types';
+import { v4, validate } from 'uuid';
 
 @Injectable()
 export class AlbumService {
-  getHello(): string {
-    return 'Hello World!';
+  private albums: Album[] = [];
+
+  getAll() {
+    return this.albums;
+  }
+
+  getOne(id: string) {
+    const foundedAlbum = this.findAlbum(id);
+    if (!validate(id)) {
+      throw new HttpException('id is not valid', HttpStatus.BAD_REQUEST);
+    }
+    if (!foundedAlbum) {
+      throw new HttpException('Album is not found', HttpStatus.NOT_FOUND);
+    }
+    return foundedAlbum;
+  }
+
+  create({ artistId, ...props }: ValidatedAlbum) {
+    const newAlbum = {
+      ...props,
+      artistId: artistId || null,
+      id: v4(),
+    };
+    this.albums.push(newAlbum);
+    return newAlbum;
+  }
+
+  update(updateAlbumDto: ValidatedAlbum, id: string) {
+    const foundedAlbum = this.findAlbum(id);
+
+    if (!validate(id)) {
+      throw new HttpException('id is not valid', HttpStatus.BAD_REQUEST);
+    }
+    if (!foundedAlbum) {
+      throw new HttpException('Album is not found', HttpStatus.NOT_FOUND);
+    }
+    const updatedAlbum = {
+      ...foundedAlbum,
+      updateAlbumDto,
+    };
+    this.albums = [
+      ...this.albums.filter(({ id }) => id !== foundedAlbum.id),
+      updatedAlbum,
+    ];
+    return updatedAlbum;
+  }
+
+  delete(id: string) {
+    const foundedAlbum = this.findAlbum(id);
+
+    if (!validate(id)) {
+      throw new HttpException('id is not valid', HttpStatus.BAD_REQUEST);
+    }
+    if (!foundedAlbum) {
+      throw new HttpException('Album is not found', HttpStatus.NOT_FOUND);
+    }
+
+    this.albums = this.albums.filter(({ id }) => id !== foundedAlbum.id);
+  }
+
+  findAlbum(abumId: string) {
+    return this.albums.find(({ id }) => id === abumId);
   }
 }
