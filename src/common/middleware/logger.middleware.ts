61026dd68f9e03ef1680500168035631338f526e
Zhenya Holub <zhenya.holub@rdigital.by>
2024-11-23 20:44:09 +0300

    feat: implement logging into file

--- src/common/middleware/logger.middleware.ts
+++ src/common/middleware/logger.middleware.ts
@@ -1,10 +1,11 @@
-import { HttpStatus, Injectable, Logger, NestMiddleware } from '@nestjs/common';
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
-import { writeFile } from 'fs/promises';
+import { writeLogs } from './helpers';
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   logger = new Logger('Response');
   use(req: Request, res: Response, next: NextFunction) {
+    let responseBody;
     const logger = this.logger;
     const originalSend = res.send;
     const requestData = `Request data: url - ${
@@ -15,16 +16,18 @@ export class LoggerMiddleware implements NestMiddleware {
     logger.log(requestData);
 
     res.send = function (body: any) {
-      logger.log(`Response body - `, body);
-
+      responseBody = body;
       return originalSend.call(this, body);
     };
 
     res.on('finish', async () => {
       const { statusCode, statusMessage } = res;
-      const responseData = `Response status - ${statusCode} ${statusMessage}`;
+      const responseData = `Response data: status - ${statusCode} ${statusMessage}, body - ${responseBody}`;
       logger.log(responseData);
-      //writeFile('../../../logs/logs.log', requestData + '\n' + responseData);
+      const date = new Date();
+      writeLogs(
+        `${date.toLocaleDateString()} ${date.toLocaleTimeString()}\n${requestData}\n${responseData}`,
+      );
     });
 
     next();
